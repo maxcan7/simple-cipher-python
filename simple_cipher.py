dfbd415805aefd035534dc2f463016cac7acf4dd
maxcan7 <maxcan7@gmail.com>
2019-05-06 14:56:23 -0400

    linting and caught a bug

--- simple_cipher.py
+++ simple_cipher.py
@@ -1,30 +1,39 @@
 class Cipher:
     '''
-    A cipher object is a shift / caeser cipher encryption and decryption object. It contains two methods, encode and decode. They take a plaintext string as a mandatory input, and shift_distance as an optional input. If shift_distance is not provided, it will default to 1.
+    A cipher object is a shift / caeser cipher encryption and decryption
+    object. It contains two methods, encode and decode. They take a plaintext
+    string as a mandatory input, and shift_distance as an optional input. If
+    shift_distance is not provided, it will default to 1.
     '''
-    
-    def encode(self, plaintext, shift_distance = None):
+
+    def encode(self, plaintext, shift_distance=None):
         ciphertext = []
         if shift_distance is None:
             shift_distance = 1
         for char in plaintext:
             if ord(char) >= 97 and ord(char) <= 122:
-                ciphertext.append(chr((ord(char) + shift_distance - 97) % 26 + 97))
+                ciphertext.append(chr((ord(char) + shift_distance - 97)
+                                      % 26 + 97))
             elif ord(char) >= 65 and ord(char) <= 90:
-                ciphertext.append(chr((ord(char) + shift_distance - 66) % 26 + 97 + shift_distance))
-            else: 
-                raise Exception('plaintext should include only lowercase or uppercase characters')
+                ciphertext.append(chr((ord(char) + shift_distance - 65)
+                                      % 26 + 97))
+            else:
+                raise Exception('plaintext should include only \
+                                lowercase or uppercase characters')
         return ''.join(ciphertext)
-        
-    def decode(self, plaintext, shift_distance = None):
+
+    def decode(self, plaintext, shift_distance=None):
         ciphertext = []
         if shift_distance is None:
             shift_distance = 1
         for char in plaintext:
             if ord(char) >= 97 and ord(char) <= 122:
-                ciphertext.append(chr((ord(char) - shift_distance - 97) % 26 + 97))
+                ciphertext.append(chr((ord(char) - shift_distance - 97)
+                                      % 26 + 97))
             elif ord(char) >= 65 and ord(char) <= 90:
-                ciphertext.append(chr((ord(char) - shift_distance - 66) % 26 + 97 + shift_distance))
-            else: 
-                raise Exception('plaintext should include only lowercase or uppercase characters')
+                ciphertext.append(chr((ord(char) - shift_distance + 65)
+                                      % 26 + 97))
+            else:
+                raise Exception('plaintext should include only lowercase \
+                                or uppercase characters')
         return ''.join(ciphertext)
