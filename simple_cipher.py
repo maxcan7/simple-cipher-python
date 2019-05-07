71741ff5e501da44470307b75372c0701b3d1697
maxcan7 <maxcan7@gmail.com>
2019-05-07 16:06:18 -0400

    Changed how spaces or empty / no inputs are handled. Added backwards shift support

--- simple_cipher.py
+++ simple_cipher.py
@@ -6,31 +6,47 @@ class Cipher:
     shift_distance is not provided, it will default to 1.
     '''
 
-    def encode(self, plaintext, shift_distance=None):
+    def encode(self, plaintext="", shift_distance=None):
+        if not plaintext:
+            raise Exception('Cipher.encode() requires a non-empty string as input')
         ciphertext = []
         if shift_distance is None:
             shift_distance = 1
         for char in plaintext:
-            if ord(char) >= 97 and ord(char) <= 122:
+            if ord(char) >= 97 and ord(char) <= 122 and shift_distance >= 0:
                 ciphertext.append(chr((ord(char) + shift_distance - 97)
                                       % 26 + 97))
-            elif ord(char) >= 65 and ord(char) <= 90:
+            elif ord(char) >= 65 and ord(char) <= 90 and shift_distance >= 0:
                 ciphertext.append(chr((ord(char) + shift_distance - 65)
+                                      % 26 + 97))
+            elif ord(char) >= 97 and ord(char) <= 122 and shift_distance < 0:
+                ciphertext.append(chr((ord(char) - abs(shift_distance) - 97)
+                                      % 26 + 97))
+            elif ord(char) >= 65 and ord(char) <= 90 and shift_distance < 0:
+                ciphertext.append(chr((ord(char) - shift_distance + 65)
                                       % 26 + 97))
             else:
                 raise Exception('plaintext should include only lowercase or uppercase letters')
         return ''.join(ciphertext)
 
-    def decode(self, plaintext, shift_distance=None):
+    def decode(self, plaintext="", shift_distance=None):
+        if not plaintext:
+            raise Exception('Cipher.decode() requires a non-empty string as input')
         ciphertext = []
         if shift_distance is None:
             shift_distance = 1
         for char in plaintext:
-            if ord(char) >= 97 and ord(char) <= 122:
+            if ord(char) >= 97 and ord(char) <= 122 and shift_distance >= 0:
                 ciphertext.append(chr((ord(char) - shift_distance - 97)
                                       % 26 + 97))
-            elif ord(char) >= 65 and ord(char) <= 90:
+            elif ord(char) >= 65 and ord(char) <= 90 and shift_distance >= 0:
                 ciphertext.append(chr((ord(char) - shift_distance + 65)
+                                      % 26 + 97))
+            elif ord(char) >= 97 and ord(char) <= 122 and shift_distance < 0:
+                ciphertext.append(chr((ord(char) + abs(shift_distance) - 97)
+                                      % 26 + 97))
+            elif ord(char) >= 65 and ord(char) <= 90 and shift_distance < 0:
+                ciphertext.append(chr((ord(char) + abs(shift_distance) - 65)
                                       % 26 + 97))
             else:
                 raise Exception('plaintext should include only lowercase or uppercase letters')
